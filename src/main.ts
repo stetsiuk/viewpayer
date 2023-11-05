e5e8e8038276569c651f97b22451f658c1814f1b
Oleksandr Stetsiuk <oleksandrstetsiuk2000@gmail.com>
2023-11-05 19:31:48 +0200

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    credentials: true,
+    origin: [...process.env.CLIENT_DOMAINS.split(',')],
+  });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
